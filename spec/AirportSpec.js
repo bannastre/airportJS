2bd0c1e71670ad66bf4d0880c41587ee25b0522f
Christopher Harrop <bannastre@gmail.com>
2017-08-05 12:04:10 +0100

    Create Airport class

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,32 +1,5 @@
-describe("Airport", function() {
-  var airport;
-  var plane;
-  var weather;
+const airport = new Airport
 
-  beforeEach(function() {
-    airport = new Airport;
-    plane = new Plane;
-    isWeatherStormy = false;
-  });
-
-  it("should show an empty list of landed planes", function(){
-    expect(airport.planesInAirport).toEqual([]);
-  });
-
-  it("should allow a plane to land", function(){
-    airport.landPlane(plane);
-    expect(airport.planesInAirport).toEqual([plane]);
-  });
-
-  it("should allow planes to take_off", function() {
-    airport.takeOffPlane(plane);
-    expect(airport.planesInAirport).toEqual([]);
-  });
-
-  it("should prevent planes from landing", function(){
-    isWeatherStormy = true;
-    expect(function() {
-      airport.landPlane(plane);
-    }).toThrowError("Not possible do you stormy weather");
-  });
-});
+describe('Airport', () => {
+  
+})
